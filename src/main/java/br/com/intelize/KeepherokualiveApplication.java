ef9ce51ed0bf51d91e154eb491c8ad75e4ba1564
Silvio Pereira <silvioper@gmail.com>
2017-08-14 21:20:33 -0300

    Changing project to Java language and Spring Boot.

---
+++ src/main/java/br/com/intelize/KeepherokualiveApplication.java
@@ -0,0 +1,12 @@
+package br.com.intelize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepherokualiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepherokualiveApplication.class, args);
+	}
+}
