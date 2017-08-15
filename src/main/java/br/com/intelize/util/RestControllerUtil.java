5c963d648a7b59e2f676dbf12fb67839a63d1270
Silvio Pereira <silvioper@gmail.com>
2017-08-15 13:57:43 -0300

    Improving code to reading easiness

--- src/main/java/br/com/intelize/util/RestControllerUtil.java
+++ src/main/java/br/com/intelize/util/RestControllerUtil.java
@@ -10,16 +10,27 @@ import java.util.Optional;
 public class RestControllerUtil {
 
     public static ResponseEntity getErrorResponse(BindingResult bindingResult) {
-        Optional<String> errors = bindingResult.getAllErrors().stream().map(ObjectError::toString).reduce((x, y) -> x + "," + y);
-        return ResponseEntity.badRequest().body((errors.orElse("")));
+        Optional<String> errors = bindingResult
+                .getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .reduce((x, y) -> x + " | " + y);
+
+        return ResponseEntity
+                .badRequest()
+                .body((errors.orElse("")));
     }
 
     public static ResponseEntity getInternalErrorResponse(Exception exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
     }
 
     public static ResponseEntity getCreatedResponse() {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
     }
 
 }
