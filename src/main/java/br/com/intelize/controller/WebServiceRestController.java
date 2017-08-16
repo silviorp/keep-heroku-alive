a083ed7a6bfdac7699156a8ad7f1910e3750a32a
Silvio Pereira <silvioper@gmail.com>
2017-08-16 14:58:59 -0300

    Fixing api urls declaration

--- src/main/java/br/com/intelize/controller/WebServiceRestController.java
+++ src/main/java/br/com/intelize/controller/WebServiceRestController.java
@@ -8,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController("/api/v1/webservices")
+@RestController
+@RequestMapping("/api/v1/webservices")
 public class WebServiceRestController {
 
     private final WebServiceService webServiceService;
